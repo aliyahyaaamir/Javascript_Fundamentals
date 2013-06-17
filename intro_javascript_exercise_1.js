905e4d46a7c2133dcf77521d9e1e3296e1d8efc0
ali aamir <ali_aamir@hotmail.com>
2013-06-17 13:23:38 -0500

    Completed 3rd part of the first exercise

--- intro_javascript_exercise_1.js
+++ intro_javascript_exercise_1.js
@@ -1,11 +1,11 @@
 var num_of_children;
 var partner_name;
-var location;
+var location1;
 var job_title;
 
 num_of_children = 5;
-partner_name = "Marry";
-location = "San Francisco";
-job_title = "Developer";
+partner_name = 'Marry';
+location1 = 'San Francisco';
+job_title = 'Developer';
 
-console.log("You will be a " + job_title + " in " + location + ", and married to " + partner_name + " with " + num_of_children + " kids.");
+console.log('You will be a ' + job_title + ' in ' + location1 + ', and married to ' + partner_name + ' with ' + num_of_children + ' kids.');
---
+++ intro_javascript_exercise_3.js
@@ -0,0 +1,5 @@
+var current_age = 18;
+var max_age = 105;
+var amount_per_day = 2;
+var total = (max_age-current_age)*2;
+console.log('You will need ' + total + ' to last you until the ripe old age of ' + max_age +'.');
