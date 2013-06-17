fee5eb05adb196b9e2ecebe57b7e48181e306ed5
ali aamir <ali_aamir@hotmail.com>
2013-06-17 12:26:55 -0500

    Completed 1st part of the first exercise

---
+++ intro_javascript_exercise_1.js
@@ -0,0 +1,11 @@
+var num_of_children;
+var partner_name;
+var location;
+var job_title;
+
+num_of_children = 5;
+partner_name = "Marry";
+location = "San Francisco";
+job_title = "Developer";
+
+console.log("You will be a " + job_title + " in " + location + ", and married to " + partner_name + " with " + num_of_children + " kids.")
