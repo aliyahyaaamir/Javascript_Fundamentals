4120ff04e3ea4e5bccbc536bcb21670ee134e999
ali aamir <ali_aamir@hotmail.com>
2013-06-17 13:42:49 -0500

    Completed 1st part of the second exercise for intro to javascript

---
+++ intro_javascript_exercise2_1.js
@@ -0,0 +1,10 @@
+function tellFortune(num_of_children, partner_name, geo_loc, job_title){
+
+	var fortune = 'You will be a ' + job_title + ' in ' + gec_loc + ', and married to ' + partner_name + ' with ' + num_of_children + ' kids.';
+	console.log(fortune)
+
+}
+
+tellFortune(2, Jess, Toronto, boxer);
+tellFortune(7, Susan, California, developer);
+tellFortune(5, Fifi, San Francisco, Engineer);
