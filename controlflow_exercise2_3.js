8c3d78146bd29ae0c8e800dc614fbd5bf0e42728
ali aamir <ali_aamir@hotmail.com>
2013-06-17 15:08:00 -0500

    Completed 3rd part of the second exercise for control flows and arrays

---
+++ controlflow_exercise2_3.js
@@ -0,0 +1,18 @@
+for (i = 60; i <= 100; i++){
+	if (i >= 80){
+		console.log('You got an A');
+	}
+	else if (i < 80 && i >= 70){
+		console.log('You got an B');
+	} 
+	else if (i < 70 && i >= 60){
+		console.log('You got an C');
+	}
+	else if (i < 60 && i <= 50){
+		console.log('You got an D');
+	}
+	else{
+		console.log('You got an F');
+	}
+}
+
